f52340f352802a46faf14badf76f76fa121447b9
Angel Forneiro <nforneirorosas@gmail.com>
2025-11-19 21:34:50 -0300

    Scripts

---
+++ scripts.js
@@ -0,0 +1,4 @@
+const tickets = document.getElementById("tickets");
+const contact = document.getElementById("contact");
+const getTicketsButton = document.getElementById("get-tickets");
+const sendMessage = document.getElementById("send-message");
